b2ca55a3fcce6c7f19c9e6038d0081cde606d7d1
saadfrhan <saadfarhan347@gmail.com>
2024-04-24 11:36:49 +0500

    Update preferences.ts

--- api/routes/preferences.ts
+++ api/routes/preferences.ts
@@ -5,40 +5,50 @@ import Preferences from "../models/preferences";
 const router = Router();
 
 router.post("/:id", async (req, res) => {
-  const { id: user_id } = req.params;
-  const { genres, rating } = req.body;
-  const userExist = await clerkClient.users.getUser(user_id);
+  try {
+    const { id: user_id } = req.params;
+    const { genres, rating } = req.body;
+    const userExist = await clerkClient.users.getUser(user_id);
 
-  if (!userExist) {
-    return res.status(404).json({ message: "User not found" });
-  }
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  const alreadyExist = await Preferences.findOne({ user_id });
+    const alreadyExist = await Preferences.findOne({ user_id });
 
-  if (alreadyExist) {
-    await Preferences.findOneAndUpdate({ user_id }, { genres, rating });
-    return res.status(200).json({ message: "Preferences updated" });
-  } else {
-    await Preferences.create({ user_id, genres, rating });
-    return res.status(201).json({ message: "Preferences created" });
+    if (alreadyExist) {
+      await Preferences.findOneAndUpdate({ user_id }, { genres, rating });
+      return res.status(200).json({ message: "Preferences updated" });
+    } else {
+      await Preferences.create({ user_id, genres, rating });
+      return res.status(201).json({ message: "Preferences created" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error", error });
   }
 });
 
 router.get("/:id", async (req, res) => {
-  const { id: user_id } = req.params;
-  const userExist = await clerkClient.users.getUser(user_id);
+  try {
+    const { id: user_id } = req.params;
+    const userExist = await clerkClient.users.getUser(user_id);
 
-  if (!userExist) {
-    return res.status(404).json({ message: "User not found" });
+    if (!userExist) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const preferences = await Preferences.findOne({ user_id });
+
+    if (!preferences) {
+      return res.status(404).json({ message: "Preferences not found" });
+    }
+
+    return res.status(200).json(preferences);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error", error });
   }
-
-  const preferences = await Preferences.findOne({ user_id });
-
-  if (!preferences) {
-    return res.status(404).json({ message: "Preferences not found" });
-  }
-
-  return res.status(200).json(preferences);
 });
 
 export default router;
